e8e767ca2bded1a965bd25ce0afd078b94873625
lpbono <lpbono34@gmail.com>
2021-09-30 10:23:58 -0300

    Practica1

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module practicasjava2021 {
+}
---
+++ src/practicasjava2021/Practicauno.java
@@ -0,0 +1,16 @@
+package practicasjava2021;
+
+public class Practicauno {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("\n\n\nNombre\tApellido\tEmail\t\t\tTelefono");
+		System.out.println("------\t-------\t\t-----\t\t\t--------");
+		System.out.println("\nJuan\tPerez\t\tjuanp@gmail.com\t\t451245");
+		System.out.println("\nPedro\tPardo\t\tjuanp@gmail.com\t\t451245");
+		
+
+	}
+
+}
