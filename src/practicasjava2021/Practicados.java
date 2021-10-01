43a4fdcc3ef679641d82243ed3f05f944cb92c9e
lpbono <lpbono34@gmail.com>
2021-10-01 09:22:20 -0300

    Create Practicados.java
    Ejercicio 1 y 2

---
+++ src/practicasjava2021/Practicados.java
@@ -0,0 +1,18 @@
+package practicasjava2021;
+
+public class Practicados {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n\n\nTecla de escape\t\tsignificado");
+		System.out.println("\n\\n\t\t\tsignifica nueva linea");
+		System.out.println("\n\\t\t\t\tsignifica un tab de espacio");
+		System.out.println("\n\\\"\t\t\tes para poner “ (comillas dobles) dentro del texto por ejemplo\r\n"
+				+ "\t\t\t“Belencita”");
+		System.out.println("\n\\\\\t\t\tse utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\\r\n");
+		System.out.println("\n\\´\t\t\tse utiliza para las ‘(comilla simple) para escribir por ejemplo ‘Princesita’\r\n");
+		
+
+	}
+
+}
